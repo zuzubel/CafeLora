79081c2a6416860445a09a1f6ab38305c6942305
Zuzana Belaskova <z.belaskova@gmail.com>
2020-11-01 11:39:49 +0100

    Nav bar hides and shows on click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import './index.html';
 import './style.css';
 
+const btnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+const menuAtrElm = document.querySelectorAll('.menu-a');
+
+//funkce která schová a otevírá menu
+const hideNav = () => {
+  navElm.classList.toggle('nav-closed');
+};
+
+//na klik na menu se otevře a schová
+btnElm.addEventListener('click', hideNav);
+
+//po kliknutí na jednotlivý odkaz se menu schová
+menuAtrElm.forEach((e) => {
+  e.addEventListener('click', () => {
+    hideNav();
+  });
+});
+
+
+
 
